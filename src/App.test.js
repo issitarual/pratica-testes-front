43fae42264954233879bc7a3855495397d5b3156
Laura <laura.tracierra@gmail.com>
2021-08-20 09:18:15 -0300

    testing new elements and snap

--- src/App.test.js
+++ src/App.test.js
@@ -1,60 +1,77 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react'
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import App, { calcularNovoSaldo } from "./App";
 
-describe('Componente principal', () => {
-    describe('Quando abro o App do banco', () => {
-        it('Quando abro o App do banco, o nome é exibido', () => {
-            render(<App />);
-    
-            expect(screen.getByText('ByteBank')).toBeInTheDocument();
-        })
-    
-        it('Quando abro o App do banco, o saldo é exibido', () => {
-            render(<App />);
-    
-            expect(screen.getByText('Saldo:')).toBeInTheDocument();
-        })
-    
-        it('Quando abro o App do banco, o botão de realizar transação é exibido', async () => {
-            render(<App />);
-    
-            expect(screen.getByText('Realizar operação')).toBeInTheDocument();
-        })
-    })
-    describe('Quando eu realizo uma transação', () => {
-        it('que é o saque, o valor vai diminuir', () => {
-            const valores = {
-                transacao: 'saque',
-                valor: 50
-            };
+describe("Componente principal", () => {
+  describe("Quando abro o App do banco", () => {
+    it("Quando abro o App do banco, o nome é exibido", () => {
+      render(<App />);
 
-            const novoSaldo = calcularNovoSaldo(valores, 150);
+      expect(screen.getByText("ByteBank")).toBeInTheDocument();
+    });
 
-            expect(novoSaldo).toBe(100);
-        })
+    it("Quando abro o App do banco, o saldo é exibido", () => {
+      render(<App />);
 
-         it('que é o saque, o valor vai ficar negativo', async () => {
-            const valores = {
-                transacao: 'saque',
-                valor: 50
-            };
-            
-            const novoSaldo = await calcularNovoSaldo(valores, 0);
+      expect(screen.getByText("Saldo:")).toBeInTheDocument();
+    });
 
-            expect(novoSaldo).toBe(-50);
-        }) 
+    it("Quando abro o App do banco, o botão de realizar transação é exibido", async () => {
+      render(<App />);
 
-        it('que é o deposito, o valor vai ficar positivo', () => {
-            const valores = {
-                transacao: 'deposito',
-                valor: 50
-            };
-            
-            const novoSaldo = calcularNovoSaldo(valores, 100);
+      expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+  describe("Quando eu realizo uma transação", () => {
+    it("que é o saque, o valor vai diminuir", () => {
+      const valores = {
+        transacao: "saque",
+        valor: 50,
+      };
 
-            expect(novoSaldo).toBe(150);
-        })
-    })
-})
+      const novoSaldo = calcularNovoSaldo(valores, 150);
+
+      expect(novoSaldo).toBe(100);
+    });
+
+    it("que é o saque, o valor vai ficar negativo", async () => {
+      const valores = {
+        transacao: "saque",
+        valor: 50,
+      };
+
+      const novoSaldo = await calcularNovoSaldo(valores, 0);
+
+      expect(novoSaldo).toBe(-50);
+    });
+
+    it("que é o deposito, o valor vai ficar positivo", () => {
+      const valores = {
+        transacao: "deposito",
+        valor: 50,
+      };
+
+      const novoSaldo = calcularNovoSaldo(valores, 100);
+
+      expect(novoSaldo).toBe(150);
+    });
+
+    it("que é um saque, a transação deve ser realizada", () => {
+      render(<App />);
+
+      const saldo = screen.getByText("R$ 1000");
+      const transacao = screen.getByLabelText("Saque");
+      const valor = screen.getByTestId("valor");
+      const botaoTransacao = screen.getByText("Realizar operação");
+
+      expect(saldo.textContent).toBe("R$ 1000");
+
+      fireEvent.click(transacao, { target: { value: "saque" } });
+      fireEvent.change(valor, { target: { value: 10 } });
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe("R$ 990");
+    });
+  });
+});
